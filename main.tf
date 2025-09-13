3b59d0532827bd8a196da6b2fa5a094fcec4b9b1
Philip Ndungu <pndungu2006@gmail.com>
2025-09-13 17:53:11 +0300

    Terraform Variables inside main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,19 @@ provider "aws" {
 region = "us-east-1"
 }
 
+# Data Source for ASG
+data "aws_vpc" "default" {
+default = true
+}
+
+# get the ID of the VPC from the aws
+data "aws_subnets" "default" {
+vpc_id = data.aws_vpc.default.id
+}
+
 # Security group for the instance.
 resource "aws_security_group" "instance" {
- name		= "terraform-example-instance"
+ name		= "terraform-example"
  description	= "Allow HTTP on port 8080"
 
 
@@ -27,22 +37,42 @@ egress {
 
 #EC2 instance
 
-resource "aws_instance" "example" {
-  ami                      = "ami-0080e4c5bc078760e"
+resource "aws_launch_configuration" "example" {
+  image_id                 = "ami-0080e4c5bc078760e"
   instance_type            = "t2.micro"
-  vpc_security_group_ids   = [aws_security_group.instance.id]	
+  security_groups          = [aws_security_group.instance.id]	
  
   user_data = <<-EOF
 	      #!/bin/bash
 	      echo "Hello, World" > index.html
 	      nohup busybox httpd -f -p ${var.server_port} &
-       	      EOF
-  
+              EOF
+
+# Required when using a launch configuration with an auto scaling group.
+# https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+lifecycle {
+  create_before_destroy = true
+}  
+
   tags = {
    Name = "terraform-example"
   }
 }
 
+#ASG Configs
+
+resource "aws_autoscaling_group" "example" {
+launch_configuration = aws_launch_configuration.example.name
+vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+min_size = 2
+max_size = 10
+
+tag {
+   key                 = "Name"
+   value               = "terraform-asg-example"
+   propagate_at_launch = true
+}
+}
 
 variable "object_example_with_error" {
 description = "An example of a structural type in Terraform with an error"
